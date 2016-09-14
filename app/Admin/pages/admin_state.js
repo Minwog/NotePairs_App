60a55883365177663906264881bd72f4d85ac6fc
Minwog <noemie.gonnier@laposte.net>
2016-09-14 15:47:36 +0200

    création routes eleve enseignant cours

--- app/Admin/pages/admin_state.js
+++ app/Admin/pages/admin_state.js
@@ -15,5 +15,37 @@
                     }
                 })
 
+                    .state('admin.students',{
+                        parent:'admin',
+                        url:'/students',
+                        views:{
+                            'content':{
+                                templateUrl:'app/Admin/pages/eleves/eleves.html',
+                                controller:'ElevesController'
+                            }
+                        }
+                    })
+
+                    .state('admin.enseignants',{
+                        parent:'admin',
+                        url:'/enseignant',
+                        views:{
+                            'content':{
+                                templateUrl:'app/Admin/pages/eleves/eleves.html',
+                                controller:'EnseignantController'
+                    }
+                }
+                    })
+
+                    .state('admin.cours',{
+                        parent:'admin',
+                        url:'/cours',
+                        views:{
+                            'content':{
+                                templateUrl:"app/Admin/pages/eleves/eleves.html"
+                            }
+                        }
+                    })
+
             }]);
 })();
