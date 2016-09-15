578249dce29fc98dfe1f1e266785acfec729fdff
Yohanan <yo_94@hotmail.fr>
2016-09-15 16:24:04 +0200

    enseignants html

--- app/Admin/pages/eleves/eleve_add_controller.js
+++ app/Admin/pages/eleves/eleve_add_controller.js
@@ -4,13 +4,16 @@
 
         .controller('AddEleveController',['$scope','$state','$stateParams', 'alerteService', 'ElevesService',
             function ($scope,$state,$stateParams,alerteService,ElevesService) {
+                    console.log("dans le contrôleur AddEleveController")
+                $scope.ElevesList = ElevesService.query();
 
 //--- Methode add pour ajouter un Eleve à la liste ---//
                 $scope.addEleve = function () {
                     $scope.Eleve = new ElevesService();
-                    $scope.Eleve.$save(function () {
+                    ElevesService.save($scope.Eleve, function () {
+                        console.log("dans la fonction save");
                         //retourner à la liste d'élèves
-                        $state.go('Eleves');
+                        $state.go('admin.students');
                     })
                 }
             }
