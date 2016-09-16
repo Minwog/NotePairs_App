33b0c6c05ef5a73e36f9518b1dde49ed07f9e509
adelbechihi <S3m;usmx>
2016-09-16 12:01:56 +0200

    Adel: Tables

---
+++ app/Admin/pages/cours/cours_controller.js
@@ -0,0 +1,4 @@
+angular.module('NotePairApp')
+    .controller('CoursController',['$scope','$state','$stateParams', 'alerteService', 'ElevesService', function ($scope,$state,$stateParams, alerteService, ElevesService) {
+
+    }]);
