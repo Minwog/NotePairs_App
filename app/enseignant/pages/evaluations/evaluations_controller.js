ed5e9f3bf4a98434ed11551983dbed4462a49356
adelbechihi <S3m;usmx>
2016-09-28 23:36:20 +0200

    Adel: eleve/evaluations

--- app/enseignant/pages/evaluations/evaluations_controller.js
+++ app/enseignant/pages/evaluations/evaluations_controller.js
@@ -103,7 +103,6 @@ angular.module('NotePairApp')
 
         LocalEvaluationsService.query().then(function (data) {
             $scope.sectionList=data;
-            console.log($scope.EvaluationsList)
         });
 
         $scope.newSection={
@@ -120,7 +119,7 @@ angular.module('NotePairApp')
 //--- Methode add pour ajouter un Eleve à la liste ---//
         $scope.addSection = function () {
             $scope.newSection.ordre = $scope.sectionList.length;
-            console.log($scope.newSection)
+            console.log($scope.sectionList[1])
             LocalEvaluationsService.save($scope.newSection);
         };
 
@@ -138,12 +137,19 @@ angular.module('NotePairApp')
             $scope.critere=i;
         }
 
-        $scope.createSection=function(){
-            $scope.evaluation.sections.push(section);
-           // localStorageService.set("sections", $scope.evaluation.sections);
+        $scope.newCritere = {
+            "id":12
         }
-        
-        $scope.createCritere=function (id) {
+        $scope.addCritere=function (sec) {
+            for(var i = 0; i < $scope.sectionList.length; i++){
+                if($scope.sectionList[i].id == sec.id){
+                    $scope.sectionList[i].criteres.push($scope.newCritere)
+                    //$scope.sectionList[i].criteres.push($scope.newCritere);
+                    //LocalEvaluationsService.save($scope.sectionList);
+                    console.log($scope.sectionList[i])
+                    break;
+                }
+            }
 
         }
 
